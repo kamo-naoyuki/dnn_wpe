310260019bb8dfdb771cc58990e96635d1ca4822
Naoyuki Kamo <naoyuki.kamo829@gmail.com>
2020-05-02 17:33:02 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,9 @@ from setuptools import setup
 setup(name='pytorch_wpe',
       version='0.0.0',
       description='A pytorch implementation of Weighted Prediction Error',
-      author='',
-      author_email='',
-      url='',
+      url='https://github.com/nttcslab-sp/dnn_wpe',
       py_modules=['pytorch_wpe'],
-      install_requires=['numpy'],
+      install_requires=['numpy', 'torch_complex'],
       setup_requires=[],
       tests_require=[]
       )
